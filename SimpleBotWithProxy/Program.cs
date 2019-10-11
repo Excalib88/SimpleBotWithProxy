e5f0b74311c5cd633cf387b25a56c90eda14e8e2
Damir Mansurov <rave9373@gmail.com>
2019-10-11 20:25:43 +0300

    Init

---
+++ SimpleBotWithProxy/Program.cs
@@ -0,0 +1,39 @@
+﻿using MihaZupan;
+using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+
+namespace SimpleBotWithProxy
+{
+    class Program
+    {
+        private static TelegramBotClient bot;
+
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            var socksProxy = new HttpToSocks5Proxy("183.102.171.77", 8888);
+            bot = new TelegramBotClient("983922268:AAGxXiplHabEGFUYv92pKe9VjogbPW2793I", socksProxy);
+
+            StartBot();
+
+            Console.ReadLine();
+        }
+
+        static void StartBot()
+        {
+            bot.OnUpdate += OnUpdate;
+            bot.StartReceiving();
+        }
+
+        public static async void OnUpdate(object sender, UpdateEventArgs e)
+        {
+            if (e.Update.Message is Message message && message.Text is string text)
+            {
+                await bot.SendTextMessageAsync(message.Chat, "Echo:\n" + text);
+
+            }
+        }
+    }
+}
